c30bb3c8371cb9bf9c8ddf24e6f8163f53cca7c6
Muddi2232 <154959246+Muddi2232@users.noreply.github.com>
2024-03-21 23:55:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// Question 1
+// for (var i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// Question 2
+// Even Numbers
+// for (var i= 1; i<=20; i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// Question 3
+// Odd Numbers
+// for (var i= 1; i<=15; i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// var str ='Hello World!'
+// var splited =str.split(" ")
+// console.log(splited)
+
+
+// for (var i=1; i<=10; i++){
+//     console.log(i*i)
+// }
+
+// var numbers = [50,40,25,60,45,258,10]
+// var smallestNumbers=0
+// for (var i=1;i< numbers.length;i++){
+//     if (numbers[i]>smallestNumbers){
+//         smallestNumbers=numbers[i]
+//     }
+//     console.log(smallestNumbers)
+// }
